7e44493866c95ffbce43bee4ae0a38c848e28b2e
Geert Bevin <gbevin@uwyn.com>
2024-02-25 07:51:45 -0500

    Added create.sh and upgrade.sh scripts that rely on curl

---
+++ src/scripts/create.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+version=$(curl -Ls -o /dev/null -w "%{url_effective}" https://github.com/rife2/bld/releases/latest)
+version=${version##*/}
+filepath=$(mktemp -u -t "bld-$version.jar")
+
+echo "Downloading bld v$version..."
+echo
+
+curl -L -s "https://github.com/rife2/bld/releases/download/$version/bld-$version.jar" -o "$filepath"
+
+echo "Welcome to bld v$version."
+java -jar "$filepath" create
+
+rm -f "$filepath"
